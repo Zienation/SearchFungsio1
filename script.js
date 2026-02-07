a15575357f314b7dcdf87dd9779db0a3e9f03edd
Zienation <rofifdhaniswarakenzie@gmail.com>
2026-02-07 09:02:59 +0700

    perubahan latar belakang

--- script.js
+++ script.js
@@ -1,56 +1,40 @@
 const dataMahasiswa = [
-// BADAN PENGURUS HIMPUNAN
-    { nim: "24051204053", nama: "Soma Zenwira WYW  ", dept: "Ketua Himpunan" },
-    { nim: "24051204155", nama: "Kenzie Rofif Dhaniswara  ", dept: "Wakil Ketua Himpunan " },
-    { nim: "24051204156", nama: "Desty Putri Puspitasari  ", dept: "BPH (Sekretaris 1)" },
-    { nim: "25051204194", nama: "Zhafarina Amalia Nur S. ", dept: "BPH (Sekretaris 2)" },
-    { nim: "24051204112", nama: "Nuris Safira Rohmah ", dept: "BPH (Bendahara 1)" },
-    { nim: "25051204252", nama: "Salsabila Putri Ariska   ", dept: "BPH (Bendahara 2)" },
-// DEPARTEMEN AKADEMIK, KEILMUAN, DAN IT
-    // PENRISTEK 
-    { nim: "24051204125", nama: "Bee Nayaka Athalah Muchamad    ", dept: "Departemen Akademik, Keilmuan, dan IT (divisi PENRISTEK)" },
-    { nim: "25051204012", nama: "Nando Abdillah Salam    ", dept: "Departemen Akademik, Keilmuan, dan IT (divisi PENRISTEK)" },
-    { nim: "25051204042", nama: "Rihhadatul Aisyikin Mardhiyyah     ", dept: "Departemen Akademik, Keilmuan, dan IT (divisi PENRISTEK)" },
-    { nim: "25051204138", nama: "Hafizh Ahmad Yusuf     ", dept: "Departemen Akademik, Keilmuan, dan IT (divisi PENRISTEK)" },
-    // KOMINFO
-    { nim: "24051204119", nama: "Mahdi Ghani Afriza   ", dept: "Departemen Akademik, Keilmuan, dan IT (divisi KOMINFO)" },
-    { nim: "25051204016", nama: "Muhammad Nur Fajri   ", dept: "Departemen Akademik, Keilmuan, dan IT (divisi KOMINFO)" },
-    { nim: "25051204041", nama: "Azzarah Andina Rohmatul Izzah   ", dept: "Departemen Akademik, Keilmuan, dan IT (divisi KOMINFO)" },
-    { nim: "25051204134", nama: "Achmad Krisna Nurvian Ananda   ", dept: "Departemen Akademik, Keilmuan, dan IT (divisi KOMINFO)" },
-
-    // PROJECT EVENT 
-    { nim: "(25051204029", nama: "Maria Elisabeth Siloy   ", dept: "Departemen Akademik, Keilmuan, dan IT (divisi PE)" },
-    { nim: "24051204111", nama: "Haidar Ali Yafi    ", dept: "Departemen Akademik, Keilmuan, dan IT (divisi PE)" },
-
-// DEPARTEMEN HUBUNGAN MASYARAKAT 
-    // DIVISI LUAR NEGERI 
-    { nim: "24051204083", nama: "Alre Ahmad Satriawan "  , dept: "Departemen Hubungan Masyarakat (Divisi Luar Negeri)" },
-    { nim: "25051204011", nama: "Albani Rasya Surya Saputra  "  , dept: "Departemen Hubungan Masyarakat (Divisi Luar Negeri)" },
-    { nim: "25051204137", nama: "Azizah Fairuz Dzakiyya"  , dept: "Departemen Hubungan Masyarakat (Divisi Luar Negeri)" },
-
-    // DIVISI DALAM NEGERI
-    { nim: "24051204211", nama: "Davin Ridho Kurniawan   " , dept: "Departemen Hubungan Masyarakat (Divisi Dalam Negeri)" },
-    { nim: "25051204105", nama: "Nur Rahman  " , dept: "Departemen Hubungan Masyarakat (Divisi Dalam Negeri)" },
-    { nim: "25051204063", nama: "Shinta Nur’aini Dwi  " , dept: "Departemen Hubungan Masyarakat (Divisi Dalam Negeri)" },
-
-// DEPARTEMEN PSDM 
-    { nim: "25051204094", nama: "Muhammad Bahrul Ulum ", dept: "Departemen PSDM (Pengembangan Sumber Daya Mahasiswa)" },
-    { nim: "25051204225", nama: "Adinda Fatihadina Islamiah ", dept: "Departemen PSDM (Pengembangan Sumber Daya Mahasiswa)" },
-    { nim: "24051204157", nama: "M. Nabil Ridho Farabi  ", dept: "Departemen PSDM (Pengembangan Sumber Daya Mahasiswa)" },
-    { nim: "24051204209", nama: "Reihan Alvarizy  ", dept: "Departemen PSDM (Pengembangan Sumber Daya Mahasiswa)" },
-// DEPARTEMEN KEWIRAUSAHAAN
-    { nim: "24051204228", nama: "Zikry Azizy Aljava ", dept: "Departemen Kewirausahaan" },
-    { nim: "25051204096", nama: "Nur Lailatul Ummah ", dept: "Departemen Kewirausahaan" },
-    { nim: "25051204008", nama: "Mohammad Naufal Wicaksono  ", dept: "Departemen Kewirausahaan" },
-// DEPARTEMEN MINAT DAN BAKAT
-    { nim: "24051204150", nama: "Muhammad Hikmal Kurniah  ", dept: "Departemen Minat dan Bakat" },
+    { nim: "24051204053", nama: "Soma Zenwira WYW", dept: "Ketua Himpunan" },
+    { nim: "24051204155", nama: "Kenzie Rofif Dhaniswara", dept: "Wakil Ketua Himpunan" },
+    { nim: "24051204156", nama: "Desty Putri Puspitasari", dept: "BPH (Sekretaris 1)" },
+    { nim: "25051204194", nama: "Zhafarina Amalia Nur S.", dept: "BPH (Sekretaris 2)" },
+    { nim: "24051204112", nama: "Nuris Safira Rohmah", dept: "BPH (Bendahara 1)" },
+    { nim: "25051204252", nama: "Salsabila Putri Ariska", dept: "BPH (Bendahara 2)" },
+    { nim: "24051204125", nama: "Bee Nayaka Athalah Muchamad", dept: "Departemen Akademik, Keilmuan, dan IT (divisi PENRISTEK)" },
+    { nim: "25051204012", nama: "Nando Abdillah Salam", dept: "Departemen Akademik, Keilmuan, dan IT (divisi PENRISTEK)" },
+    { nim: "25051204042", nama: "Rihhadatul Aisyikin Mardhiyyah", dept: "Departemen Akademik, Keilmuan, dan IT (divisi PENRISTEK)" },
+    { nim: "25051204138", nama: "Hafizh Ahmad Yusuf", dept: "Departemen Akademik, Keilmuan, dan IT (divisi PENRISTEK)" },
+    { nim: "24051204119", nama: "Mahdi Ghani Afriza", dept: "Departemen Akademik, Keilmuan, dan IT (divisi KOMINFO)" },
+    { nim: "25051204016", nama: "Muhammad Nur Fajri", dept: "Departemen Akademik, Keilmuan, dan IT (divisi KOMINFO)" },
+    { nim: "25051204041", nama: "Azzarah Andina Rohmatul Izzah", dept: "Departemen Akademik, Keilmuan, dan IT (divisi KOMINFO)" },
+    { nim: "25051204134", nama: "Achmad Krisna Nurvian Ananda", dept: "Departemen Akademik, Keilmuan, dan IT (divisi KOMINFO)" },
+    { nim: "25051204029", nama: "Maria Elisabeth Siloy", dept: "Departemen Akademik, Keilmuan, dan IT (divisi PE)" },
+    { nim: "24051204111", nama: "Haidar Ali Yafi", dept: "Departemen Akademik, Keilmuan, dan IT (divisi PE)" },
+    { nim: "24051204083", nama: "Alre Ahmad Satriawan", dept: "Departemen Hubungan Masyarakat (Divisi Luar Negeri)" },
+    { nim: "25051204011", nama: "Albani Rasya Surya Saputra", dept: "Departemen Hubungan Masyarakat (Divisi Luar Negeri)" },
+    { nim: "25051204137", nama: "Azizah Fairuz Dzakiyya", dept: "Departemen Hubungan Masyarakat (Divisi Luar Negeri)" },
+    { nim: "24051204211", nama: "Davin Ridho Kurniawan", dept: "Departemen Hubungan Masyarakat (Divisi Dalam Negeri)" },
+    { nim: "25051204105", nama: "Nur Rahman", dept: "Departemen Hubungan Masyarakat (Divisi Dalam Negeri)" },
+    { nim: "25051204063", nama: "Shinta Nur’aini Dwi", dept: "Departemen Hubungan Masyarakat (Divisi Dalam Negeri)" },
+    { nim: "25051204094", nama: "Muhammad Bahrul Ulum", dept: "Departemen PSDM (Pengembangan Sumber Daya Mahasiswa)" },
+    { nim: "25051204225", nama: "Adinda Fatihadina Islamiah", dept: "Departemen PSDM (Pengembangan Sumber Daya Mahasiswa)" },
+    { nim: "24051204157", nama: "M. Nabil Ridho Farabi", dept: "Departemen PSDM (Pengembangan Sumber Daya Mahasiswa)" },
+    { nim: "24051204209", nama: "Reihan Alvarizy", dept: "Departemen PSDM (Pengembangan Sumber Daya Mahasiswa)" },
+    { nim: "24051204228", nama: "Zikry Azizy Aljava", dept: "Departemen Kewirausahaan" },
+    { nim: "25051204096", nama: "Nur Lailatul Ummah", dept: "Departemen Kewirausahaan" },
+    { nim: "25051204008", nama: "Mohammad Naufal Wicaksono", dept: "Departemen Kewirausahaan" },
+    { nim: "24051204150", nama: "Muhammad Hikmal Kurniah", dept: "Departemen Minat dan Bakat" },
     { nim: "25051204108", nama: "Aldi Abdurrahman Nafis", dept: "Departemen Minat dan Bakat" },
-    { nim: "25051204212", nama: "Fikri Khair Rahmansyah  ", dept: "Departemen Minat dan Bakat" },
-    { nim: "25051204243", nama: "Sahira Ramadani Fitrania  ", dept: "Departemen Minat dan Bakat" },
-// DEPARTEMEN SOSIAL DAN MASYARAKAT
-    { nim: "24051204158", nama: "Wildandy Dwi Ananda   ", dept: "Departemen Sosial dan Masyarakat" },
-    { nim: "25051204135", nama: "M. Ilkliluddin Al Wafi   ", dept: "Departemen Sosial dan Masyarakat" },
-    { nim: "25051204185", nama: "Rivana Adinda Putri   ", dept: "Departemen Sosial dan Masyarakat" },
+    { nim: "25051204212", nama: "Fikri Khair Rahmansyah", dept: "Departemen Minat dan Bakat" },
+    { nim: "25051204243", nama: "Sahira Ramadani Fitrania", dept: "Departemen Minat dan Bakat" },
+    { nim: "24051204158", nama: "Wildandy Dwi Ananda", dept: "Departemen Sosial dan Masyarakat" },
+    { nim: "25051204135", nama: "M. Ilkliluddin Al Wafi", dept: "Departemen Sosial dan Masyarakat" },
+    { nim: "25051204185", nama: "Rivana Adinda Putri", dept: "Departemen Sosial dan Masyarakat" },
 ];
 
 function cariData() {
@@ -70,7 +54,6 @@ function cariData() {
         resultWrapper.style.display = "block";
         errorMsg.style.display = "none";
 
-        // Fitur: Konfeti menyebar ke seluruh website
         spreadConfetti();
     } else {
         resultWrapper.style.display = "none";
@@ -93,7 +76,6 @@ function spreadConfetti() {
 
         const particleCount = 50 * (timeLeft / duration);
         
-        // Meledak dari posisi acak di layar
         confetti(Object.assign({}, defaults, { 
             particleCount, 
             origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
